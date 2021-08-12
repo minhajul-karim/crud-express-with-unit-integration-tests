56c5f14797dc138f00108686fe5683fecaaa6fd1
minhajul-karim <minhajul.kaarim@gmail.com>
2021-08-12 17:47:17 +0600

    Integration test /customers/add POST method

--- tests/app.test.js
+++ tests/app.test.js
@@ -9,6 +9,7 @@
 const app = require("../app");
 const supertest = require("supertest");
 const request = supertest(app);
+const db = require('../models')
 
 // Test suite for testing the index route
 describe("index route", () => {
@@ -18,9 +19,9 @@ describe("index route", () => {
   });
 
   test("should respond with 404 for requesting bogus routes", async () => {
-    const response = await request.get('/foobar')
-    expect(response.status).toBe(404)
-  })
+    const response = await request.get("/foobar");
+    expect(response.status).toBe(404);
+  });
 });
 
 // Test suite for testing the customers route
@@ -38,5 +39,31 @@ describe("/customers route", () => {
   test('should contain <p class="lead">Customers</p> inside body', async () => {
     const response = await request.get("/customers");
     expect(response.text).toMatch(/<p class="lead">Customers<\/p>/);
+  });
+});
+
+// Test suite for testing the /customers/add route
+describe("/customers/add route", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true })
+  })
+
+  test("should return status code 200 visiting /customers/add route", async () => {
+    const response = await request.get("/customers/add");
+    expect(response.status).toBe(200);
+  });
+
+  test("should display the form when users visit customers/add route", async () => {
+    const response = await request.get("/customers/add");
+    expect(response.text).toMatch(
+      /<form action="\/customers\/add" method="POST">/
+    );
+  });
+
+  test.only("should redirect to homepage after creating a new user", async () => {
+    const response = await request
+      .post("/customers/add")
+      .send('name=Jobayer Mojumder&email=job@gmail.com&phone=0190909090909')
+    expect(response.status).toBe(302)
   });
 });
